308d363469ad4f87fa37c72f8f2624fe1c3ed5da
Didier Domínguez <dadu0699@hotmail.com>
2021-01-20 17:37:59 -0600

    [ADD] App Peliculas

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+import 'package:peliculas/src/widgets/card_swiper_widget.dart';
+import 'package:peliculas/src/widgets/movie_horizontal.dart';
+import 'package:peliculas/src/providers/peliculas_provider.dart';
+import 'package:peliculas/src/search/search_delegate.dart';
+
+class HomePage extends StatelessWidget {
+  final peliculasProvider = new PeliculasProvider();
+
+  @override
+  Widget build(BuildContext context) {
+    peliculasProvider.getPopulares();
+
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text('Peliculas'),
+        backgroundColor: Colors.indigoAccent,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              showSearch(
+                context: context,
+                delegate: DataSearch(),
+                // query: 'Texto precargado',
+              );
+            },
+          )
+        ],
+      ),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            _swiperTarjetas(),
+            _footer(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _swiperTarjetas() {
+    return FutureBuilder(
+      future: peliculasProvider.getEnCines(),
+      builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+        if (snapshot.hasData) {
+          return CardSwiper(peliculas: snapshot.data);
+        }
+        return Container(
+          height: 450.0,
+          child: Center(
+            child: CircularProgressIndicator(),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _footer(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(left: 20.0),
+            child: Text(
+              'Populares',
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
+          ),
+          SizedBox(height: 8.5),
+          StreamBuilder(
+            stream: peliculasProvider.popularesStream,
+            builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+              if (snapshot.hasData) {
+                return MovieHorizontal(
+                  peliculas: snapshot.data,
+                  siguientePagina: peliculasProvider.getPopulares,
+                );
+              }
+              return Center(child: CircularProgressIndicator());
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/src/search/search_delegate.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+
+import 'package:peliculas/src/models/pelicula_model.dart';
+import 'package:peliculas/src/providers/peliculas_provider.dart';
+
+class DataSearch extends SearchDelegate {
+  String seleccion = '';
+  final peliculasProvider = new PeliculasProvider();
+
+  // final peliculas = ['a', 'b', 'c', 'd'];
+  // final peliculasRecientes = ['Spiderman', 'Capitan America'];
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    // Las acciones del AppBar
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
+      )
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    // Icono a la izquierda del AppBar
+    return IconButton(
+      icon: AnimatedIcon(
+        icon: AnimatedIcons.menu_arrow,
+        progress: transitionAnimation,
+      ),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // Crea los resultados que se mostrarán
+    return Center(
+      child: Container(
+        height: 100.0,
+        width: 100.0,
+        color: Colors.pinkAccent,
+        child: Text(seleccion),
+      ),
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // Sugerencias que aparecen cuando se escribe
+    if (query.isEmpty) {
+      return Container();
+    }
+
+    return FutureBuilder(
+      future: peliculasProvider.getBuscarPelicula(query),
+      builder: (BuildContext context, AsyncSnapshot<List<Pelicula>> snapshot) {
+        if (snapshot.hasData) {
+          final peliculas = snapshot.data;
+
+          return ListView(
+            children: peliculas.map((pelicula) {
+              return ListTile(
+                leading: FadeInImage(
+                  image: NetworkImage(pelicula.getPosterImg()),
+                  placeholder: AssetImage('assets/img/no-image.jpg'),
+                  width: 50.0,
+                  fit: BoxFit.cover,
+                ),
+                title: Text(pelicula.title),
+                subtitle: Text(pelicula.originalTitle),
+                onTap: () {
+                  // close(context, null);
+                  pelicula.uniqueID = '';
+                  Navigator.pushNamed(context, 'detalle', arguments: pelicula);
+                },
+              );
+            }).toList(),
+          );
+        }
+        return Center(child: CircularProgressIndicator());
+      },
+    );
+  }
+
+  /*
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // Sugerencias que aparecen cuando se escribe
+
+    final listaSugerida = (query.isEmpty)
+        ? peliculasRecientes
+        : peliculas
+            .where((p) => p.toLowerCase().startsWith(query.toLowerCase()))
+            .toList();
+
+    return ListView.builder(
+      itemCount: listaSugerida.length,
+      itemBuilder: (context, i) {
+        return ListTile(
+          leading: Icon(Icons.movie),
+          title: Text(listaSugerida[i]),
+          onTap: () {
+            seleccion = listaSugerida[i];
+            showResults(context);
+          },
+        );
+      },
+    );
+  }
+  */
+}
